4d82e5503edb8dfbbe39630120102241eea6479e
Junsik <wnstlr4045@naver.com>
2022-08-17 00:40:23 +0900

    8월 17일 at home

---
+++ src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,18 @@
+package hello.login;
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter(){
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean =
+                new FilterRegistrationBean<>();
+    }
+}
